13676151925cbac0c87788eb09bb005eb1520dc5
yuds <darcy.u83@gmail.com>
2022-08-20 17:53:45 +0900

    before stash back

--- src/aaaFolder/AAAFile.tsx
+++ src/aaaFolder/AAAFile.tsx
@@ -2,6 +2,7 @@ import { View, Text } from 'react-native';
 import React from 'react';
 
 export default function AAAFile() {
+  console.log('haha');
   return (
     <View>
       <Text>AAAFile</Text>
